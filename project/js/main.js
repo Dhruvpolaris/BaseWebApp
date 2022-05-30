e07137b8ff5531bfc68d3600f07cfac2f58f0f53
dhruvpolaris <dhruv.phulsundar@gmail.com>
2022-05-30 21:18:14 +0530

    API Integration

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+// getWeather();
+})
 
 function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
@@ -9,3 +12,24 @@ function myFunction() {
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
 }
 
+function getWeather(searchParam){
+
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchParam+"&units=imperial&APPID="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+    // console.log(data);
+
+  }, error: function(error){
+    alert("Incorrect City Name");
+  }})
+}
+
+function searchWeather(){
+    var searchParam = $(".search").val();
+    getWeather(searchParam);
+}
